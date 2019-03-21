bdc9d3bb4459a7e7e4096cb7bde5cdae94950383
moreal <dev.moreal@gmail.com>
2019-03-21 23:13:20 +0900

    Add install.sh

---
+++ jenkins/scripts/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt-get update
+
+apt-get install jenkins
