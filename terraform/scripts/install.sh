c7c677e3f961fb934a4ac339a0bb6e5e6839f041
moreal <dev.moreal@gmail.com>
2019-03-22 23:24:39 +0900

    Update jenkins install scripts

--- terraform/scripts/install.sh
+++ terraform/scripts/install.sh
@@ -1,5 +1,6 @@
+touch testfile
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 
-apt-get update
-apt-get install jenkins
+sudo apt-get update
+sudo apt-get install openjdk-8-jre jenkins -y
