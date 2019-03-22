31f64e9b0eb0b158863dc23b242504655798dd66
moreal <dev.moreal@gmail.com>
2019-03-22 10:32:12 +0900

    Add output for internal_ip

--- jenkins/output.tf
+++ jenkins/output.tf
@@ -1,3 +1,7 @@
 output "public_ip" {
   value = "${google_compute_instance.jenkins.network_interface.0.network_ip}"
 }
+
+output "internal_ip" {
+  value = "${google_compute_address.jenkins-internal-ip.address}"
+}
