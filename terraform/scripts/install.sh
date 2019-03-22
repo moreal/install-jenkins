b7f26c8e5702a9169f71fc9d15438ef2d7f1dec8
moreal <dev.moreal@gmail.com>
2019-03-22 14:45:40 +0900

    Rename jenkins -> terraform, ansible will be added

--- terraform/scripts/install.sh
+++ terraform/scripts/install.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
-
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-apt-get update
 
+apt-get update
 apt-get install jenkins
